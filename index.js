c98cec81e2848b91eba0b6b7bbfac61bfde7ce5a
Richard Tinajero <rj.tinajero92@gmail.com>
2019-02-26 17:16:16 -0800

    assignmt done

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const terminalCommands = require('./terminal-commands');
 
 terminalCommands.ls();
+
+terminalCommands.touch();
+
+terminalCommands.mkdir();
--- terminal-commands.js
+++ terminal-commands.js
@@ -1,3 +1,13 @@
+//Questions
+//1. Module inports are by using filenames and a require method. 
+//2. You can use export statement
+ 
+//Online Research Questions
+//1.  You can use module.exports or the exports object. 
+//2. Modules is just a chunk of JavaScript code written in a file. ES6 primarily use import/export commands to pull in modules from files like when used in react. 
+
+
+
 const fs = require('fs');
 
 module.exports.ls = () => {
@@ -9,3 +19,16 @@ module.exports.ls = () => {
         console.log(filesToString);
     });
 };
+
+module.exports.touch = () => {
+    fs.writeFile('new_file.txt', '', (err) => {
+        if(err) return err;
+        console.log('Saved!');
+    }); 
+  };
+  
+  module.exports.mkdir = () => {
+    fs.mkdir('/', {recursive: true}, (err) => {
+        if(err) return err;
+    });
+  };
